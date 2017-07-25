36fa232570a0e3e126cf825e320d5876b105fef7
Anderson Ferreira Câmara <andersonferreira1277@gmail.com>
2017-07-25 15:09:42 -0300

    mais alguns exercicios

---
+++ src/absTest/AbsTest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package absTest;
+
+/**
+ *
+ * @author Anderson Ferreira Câmara (andersonferreira1277@gmail.com)
+ */
+public class AbsTest {
+    public static void main(String[] args) {
+        int x = -11;
+        int y = 1;
+        System.out.println(Math.abs(x-y));
+    }
+}
---
+++ src/bancoDeDados/PreparedStament/ModeloPreparedStament.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bancoDeDados.PreparedStament;
+
+/**
+ *
+ * @author Anderson Ferreira Câmara (andersonferreira1277@gmail.com)
+ */
+public class ModeloPreparedStament {
+
+    /*public void cdAtendente(Funcionario na) {
+    try {
+    
+    Connection abrirConx = ConexaoSQL.getInstance().getConnection();
+    
+    String sql = "INSERT INTO funcionario(nome, cpf, id_cargoFK) VALUES (?, ?, ?);";
+    
+    PreparedStatement stat = abrirConx.prepareStatement(sql);
+    stat.setString(1, na.getNome());
+    stat.setString(2, na.getCPF());
+    stat.setInt(3, 2);
+    
+    stat.execute();
+    stat.close();
+    
+    } catch (Exception e) {
+    e.printStackTrace();
+    }
+    
+    }*/
+}
