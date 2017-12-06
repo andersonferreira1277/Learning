96e9a0f7f06a989349a6832eb794672943f11ff1
Anderson Ferreira Câmara <andersonferreira1277@gmail.com>
2017-12-06 16:26:37 -0300

    Add more files.

---
+++ src/DemoList/DemoList.java
@@ -0,0 +1,15 @@
+package DemoList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DemoList {
+
+	public static void main(String[] args) {
+		List<Integer> teste = new ArrayList<Integer>();
+		teste.add(5);
+		System.out.println(teste);
+
+	}
+
+}
